09ce431eb6d0cd3fb75db9313deafa85bdffcfeb
Kareem Mostafa <kareemabdallah@hotmail.com>
2020-01-22 04:08:08 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,52 +1,45 @@
 package main
 
 import (
-	"encoding/json" // import encoding/json package
-	"fmt"           // import formatted I/O
-	"io/ioutil"     // import I/O utilty functions
-	"log"           // import logging
-	"net/http"      // import HTTP implem.
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
-	"github.com/gorilla/mux" // HTTP router library
+	"github.com/gorilla/mux"
 )
 
-// function for handling incoming request URL to match given paths
+type Articles struct {
+	Articles []Article `json:"articles"`
+}
+
+type Article struct {
+	id      string `json:"id"`
+	message string `json:"message"`
+}
+
 func GetArticles(w http.ResponseWriter, r *http.Request) {
 
-	// Open our jsonFile as a byte array
 	byteValue, _ := ioutil.ReadFile("db.json")
+	data := json.RawMessage(string(byteValue)) // declaring a data string parsing data as raw
+	json.NewEncoder(w).Encode(&data)
 
-	// declaring a data string parsing data as raw
-	data := json.RawMessage(string(byteValue))
+	var generic map[string]interface{}
+	err := json.Unmarshal([]byte(byteValue), &generic)
 
-	// defining map function with keyvalue as a string and a value type
-	var articles map[string]*json.RawMessage
-
-	// Unmarshal func parses the JSON-encoded data and stores result in a Go value
-	// unmarshalling byteArray which contains jsonFile's content and stores it into 'articles'
-	err := json.Unmarshal(data, &articles)
-	// if it returns an error then handle it
 	if err != nil {
 		fmt.Println(err)
 	}
-	// print articles content
-	fmt.Println(articles)
 
-	// encoding our articles array into a JSON string
-	json.NewEncoder(w).Encode(articles)
+	fmt.Println(generic["articles"])
+
 }
 
 func main() {
 
-	// Defining our HTTP request multiplexer
-	router := mux.NewRouter()
-
-	// Registing URL path /articles where acquiring all articles
+	router := mux.NewRouter() // HTTP request multiplexer
 	router.HandleFunc("/articles", GetArticles).Methods("GET")
-
-	// printing Listening on localhost:9000
 	log.Println("Listening on localhost:9000")
-
-	// listening on port 9000
 	log.Fatal(http.ListenAndServe(":9000", router))
 }
